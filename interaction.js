ebdced0cfd2abbd56e6c1b5774aa044e867e8eca
david-campos <david.campos.r96@gmail.com>
2021-01-19 19:59:03 +0100

    Now the background image is selected dynamically depending on the client screen size

--- interaction.js
+++ interaction.js
@@ -7,6 +7,14 @@ function clamp(value, min, max) {
     return Math.min(Math.max(value, min), max);
 }
 
+const BACKGROUNDS = [
+    {width: 500, height: 650, url: 'img/background_3_500.png'},
+    {width: 500, height: 1000, url: 'img/background_3_500_1000.png'},
+    {width: 1000, height: 650, url: 'img/background_3_1000.png'},
+    {width: 1500, height: 975, url: 'img/background_3_1500.png'},
+    {width: 2101, height: 1365, url: 'img/background_3.png'}
+];
+
 updateMenuIcons = function () {
     if (links.length < 1) return;
 
@@ -50,6 +58,18 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
     updateMenuIcons();
+
+    // background choice
+    const width = window.screen.width;
+    const height = window.screen.height;
+    let chosen = BACKGROUNDS[BACKGROUNDS.length - 1]; // in case no size fits, take biggest
+    for (const bg of BACKGROUNDS) {
+        if (bg.width >= width && bg.height >= height) {
+            chosen = bg;
+            break;
+        }
+    }
+    document.body.style.backgroundImage = `url(${chosen.url})`;
 });
 
 window.addEventListener("scroll", updateMenuIcons);
